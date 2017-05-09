43b26bb68687f8a65df427e5c5e580a58b5b3bd9
yunfeihuang <yunfei.huang@qq.com>
2017-05-09 15:16:09 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,11 @@
 const fetch = window.fetch
 
 export default {
+  global () {
+    return fetch('/global').then((res) => {
+      return res.json()
+    })
+  },
   cnode: {
     list (query) {
       return fetch(`https://cnodejs.org/api/v1/topics?page=${query.page}&tab=${query.tab}`).then((res) => {
