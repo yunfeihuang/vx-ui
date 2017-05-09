7d3a54fcd2f11d57b53ef199702df54b9bd2d663
yunfeihuang <yunfei.huang@qq.com>
2017-05-09 14:41:26 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,22 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import actions from './actions'
 import getters from './getters'
+import mutations from './mutations'
 import cnode from './modules/cnode'
 
 Vue.use(Vuex)
 
+let state = {
+  global: {}
+}
+
 const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
+  state,
   actions,
   getters,
+  mutations,
   modules: {
     cnode: cnode
   },
