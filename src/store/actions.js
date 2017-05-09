d8c8cf14abb8fd291a444f273943b0c65b88d4e2
yunfeihuang <yunfei.huang@qq.com>
2017-05-09 15:10:05 +0800

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,7 +4,7 @@ import api from '../api'
 export default {
   global: ({commit, state}) => {
     api.global().then((json) => {
-      commit(types.GLOBAL,{data:json})
+      commit(types.GLOBAL, {data: json})
     })
   }
 }
