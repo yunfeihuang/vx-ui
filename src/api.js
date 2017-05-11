791cc31e90ac3e24ceae7687e4ef87c171878be4
黄运飞 <huangyf@77ircloud.com>
2017-05-11 19:17:55 +0800

    update api

--- src/api.js
+++ src/api.js
@@ -1,26 +1,38 @@
-const fetch = window.fetch
+const fetch = (url) => {
+  let xhr = new XMLHttpRequest()
+  return new Promise((resolve, reject) => {
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText))
+        } else {
+          reject({success: false})
+        }
+      }
+    }
+    xhr.open('GET', url, true)
+    xhr.send(null)
+  })
+}
+
+let options = {
+  timeout: 40000,
+  mode: 'cors'
+}
 
 export default {
   global () {
-    return fetch('/global').then((res) => {
-      return res.json()
-    })
+    return fetch('/global', options)
   },
   cnode: {
     list (query) {
-      return fetch(`https://cnodejs.org/api/v1/topics?page=${query.page}&tab=${query.tab}`).then((res) => {
-        return res.json()
-      })
+      return fetch(`https://cnodejs.org/api/v1/topics?page=${query.page}&tab=${query.tab}`, options)
     },
     topic (query) {
-      return fetch(`https://cnodejs.org/api/v1/topic/${query.id}`).then((res) => {
-        return res.json()
-      })
+      return fetch(`https://cnodejs.org/api/v1/topic/${query.id}`, options)
     },
     user (query) {
-      return fetch(`https://cnodejs.org/api/v1/user/${query.id}`).then((res) => {
-        return res.json()
-      })
+      return fetch(`https://cnodejs.org/api/v1/user/${query.id}`, options)
     }
   }
 }
