5e6f9ae5a120f2a07900eff75289fa7e69eb9812
黄运飞 <huangyf1024@13322.com>
2018-01-09 09:26:22 +0800

    优化样式gulp打包

---
+++ src/components/style/gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+
+gulp.task('compile', function() {
+  return gulp.src('./src/theme/*.scss')
+    .pipe(sass.sync())
+    .pipe(autoprefixer({
+      browsers: ['ie > 9', 'last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(gulp.dest('./lib/theme'));
+});
+
+gulp.task('copyfont', function() {
+  return gulp.src('./src/iconfont/**')
+    .pipe(cssmin())
+    .pipe(gulp.dest('./lib/iconfont'));
+});
+
+gulp.task('build', ['compile', 'copyfont']);
