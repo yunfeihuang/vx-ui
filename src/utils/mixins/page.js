02c06c66e39939df880751259f1eb59c2c0ae7b5
黄运飞 <huangyf1024@13322.com>
2018-06-22 10:33:23 +0800

    优化page mixins

--- src/utils/mixins/page.js
+++ src/utils/mixins/page.js
@@ -5,8 +5,10 @@ const parent = {
         vue.$el.style.opacity = '1'
         vue.$el.style.willChange = 'transform'
         vue.$el.style.transform = 'translate3d(0, 0, 0)'
+        this.$el.style.pointerEvents = 'none'
         setTimeout(() => {
           requestAnimationFrame(() => {
+            this.$el.style.pointerEvents = ''
             vue.$el.style.willChange = ''
             vue.pageState.into = true
             if (vue.$options.$mounted) {
@@ -38,11 +40,17 @@ const children = {
     }
   },
   beforeRouteLeave (to, from, next) {
-    this.$el.style.transform = 'translate3d(100%, 0, 0)'
-    this.$el.style.willChange = 'transform'
-    setTimeout(() => {
-      requestAnimationFrame(next)
-    }, 320)
+    requestAnimationFrame(() => {
+      this.$parent.$el.style.pointerEvents = 'none'
+      this.$el.style.transform = 'translate3d(100%, 0, 0)'
+      this.$el.style.willChange = 'transform'
+      setTimeout(() => {
+        requestAnimationFrame(() => {
+          this.$parent.$el.style.pointerEvents = ''
+          next()
+        })
+      }, 320)
+    })
   }
 }
 
