783b8b5a900d726ab468710a39b55967872bfffe
yunfei <yunfei.huang@qq.com>
2018-07-08 17:06:24 +0800

    优化示例

--- src/utils/mixins/page.js
+++ src/utils/mixins/page.js
@@ -1,3 +1,5 @@
+let isInIframe = window.top !== window
+let timer = isInIframe ? 0 : 320
 const parent = {
   methods: {
     pageLoad (vue) {
@@ -5,6 +7,7 @@ const parent = {
         vue.$el.style.opacity = '1'
         vue.$el.style.willChange = 'transform'
         vue.$el.style.transform = 'translate3d(0, 0, 0)'
+        vue.$el.style.transition = isInIframe ? 'none' : ''
         this.$el.style.pointerEvents = 'none'
         setTimeout(() => {
           requestAnimationFrame(() => {
@@ -21,7 +24,7 @@ const parent = {
               }
             }
           })
-        }, 320)
+        }, timer)
       })
     }
   }
@@ -44,12 +47,13 @@ const children = {
       this.$parent.$el.style.pointerEvents = 'none'
       this.$el.style.transform = 'translate3d(100%, 0, 0)'
       this.$el.style.willChange = 'transform'
+      this.$el.style.transition = isInIframe ? 'none' : ''
       setTimeout(() => {
         requestAnimationFrame(() => {
           this.$parent.$el.style.pointerEvents = ''
           next()
         })
-      }, 320)
+      }, timer)
     })
   }
 }
