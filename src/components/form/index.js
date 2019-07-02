1148ea76f75e2ab521edaca713cb8c4659e20a2d
yunfei <yunfei.huang@qq.com>
2019-07-02 10:09:30 +0800

    优化表单

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,46 +1,56 @@
 import Form from 'element-ui/lib/form'
 import FormItem from 'element-ui/lib/form-item'
 
-if (!Form.beforeMount) {
-  Form.beforeMount = function () {
-    this.$nextTick(function () {
-      let self = this
-      requestAnimationFrame(function () {
-        self.$el.classList.add('vx-form')
-      })
-    })
-  }
+if (!Form.mixins) {
+  Form.mixins = []
 }
-if (!Form.updated) {
-  Form.updated = function () {
+Form.mixins.push({
+  mounted () {
+    let self = this
+    requestAnimationFrame(function () {
+      self.$el.classList.add('vx-form')
+    })
+  },
+  updated () {
     let self = this
     requestAnimationFrame(function () {
       self.$el.classList.add('vx-form')
     })
   }
+})
+
+if (!FormItem.mixins) {
+  FormItem.mixins = []
 }
-FormItem.props.divider = {
-  type: Boolean,
-  default: false
-}
-if (!FormItem.beforeMount) {
-  FormItem.beforeMount = function () {
-    let self = this
-    this.$nextTick(function () {
-      requestAnimationFrame(function () {
-        self.$el.classList.add('vx-form-item')
-      })
-    })
-  }
-}
-if (!FormItem.updated) {
-  FormItem.updated = function () {
+
+FormItem.mixins.push({
+  props: {
+    divider: {
+      type: Boolean
+    },
+    block: {
+      type: Boolean
+    }
+  },
+  mounted () {
     let self = this
     requestAnimationFrame(function () {
       self.$el.classList.add('vx-form-item')
+      if (self.block) {
+        self.$el.classList.add('is-block')
+      }
+    })
+  },
+  updated () {
+    let self = this
+    requestAnimationFrame(function () {
+      self.$el.classList.add('vx-form-item')
+      if (self.block) {
+        self.$el.classList.add('is-block')
+      }
     })
   }
-}
+})
 
 export {
   Form,
