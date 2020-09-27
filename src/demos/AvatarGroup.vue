bcce43ae2b1879751bbac6624422dda9543e9c71
yunfei <yunfei.huang@qq.com>
2020-09-27 09:21:24 +0800

    优化

--- src/demos/AvatarGroup.vue
+++ src/demos/AvatarGroup.vue
@@ -6,14 +6,14 @@
         <avatar-group>
           <!--需要a>img 标签结构-->
           <a v-for="item in 5" :key="item">
-            <img src="https://avatars3.githubusercontent.com/u/1342149?s=88&u=f5dff8f2b904e4694574af75740cb66ac5b54c5d&v=4" alt="" />
+            <img src="/static/images/avatar.png"/>
           </a>
           <span slot="append">+888</span>
         </avatar-group>
         <br />
         <avatar-group>
           <a v-for="item in 15" :key="item">
-            <img src="https://avatars3.githubusercontent.com/u/1342149?s=88&u=f5dff8f2b904e4694574af75740cb66ac5b54c5d&v=4" alt="" />
+            <img src="/static/images/avatar.png"/>
           </a>
           <span slot="append">+888</span>
         </avatar-group>
