cd4222a49e833487efae7dc5c80b41cd4d91f2e7
黄运飞 <yunfei.huang@qq.com>
2021-06-17 17:46:03 +0800

    改造

--- src/components/popover/index.vue
+++ src/components/popover/index.vue
@@ -5,12 +5,12 @@
   <teleport to="body">
     <vx-popup
       :class="popoverClass"
-      v-model:open="open"
+      v-model:open="myOpen"
       :history="histroy"
       :opacity="overlayOpacity"
-      @close="open = false">
+      @close="handleClose">
       <template v-slot:inner>
-        <div class="vx-popover--content" ref="popoverContent">
+        <div class="vx-popover--content" ref="popoverContent" @click="handleClose">
           <slot></slot>
         </div>
       </template>
@@ -19,11 +19,17 @@
 </template>
 
 <script>
-import {createApp} from 'vue'
+import {nextTick, ref, watch} from 'vue'
 import VxPopup from '../popup'
 export default {
   name: 'VxPopover',
+  components: {
+    VxPopup
+  },
   props: {
+    open: {
+      type: Boolean
+    },
     histroy: {
       type: Boolean,
       default: true
@@ -35,19 +41,21 @@ export default {
       type: String
     }
   },
-  data () {
+  setup (props, { emit }) {
+    const popoverContent = ref(null)
+    const myOpen = ref(props.open)
+    watch(() => props.open, val => {
+      myOpen.value = val
+    })
     return {
-      open: false
-    }
-  },
-  methods: {
-    handleClick (e) {
-      this.open = true
-      this.$nextTick(() => {
-        if (this.$refs.popoverContent) {
+      popoverContent,
+      myOpen,
+      handleClick (e) {
+        myOpen.value = true
+        nextTick(() => {
           let width = window.innerWidth
           let height = window.innerHeight
-          let node = this.$refs.popoverContent
+          let node = popoverContent.value
           let rect = e.currentTarget.getBoundingClientRect()
           let left = rect.left + 'px'
           let right = 'auto'
@@ -66,96 +74,11 @@ export default {
             left === 'auto' && node.classList.add('vx-popover--content-right')
             top === 'auto' && node.classList.add('vx-popover--content-bottom')
           })
-        }
-      })
-    },
-    handleClick1 () {
-      let popover = this
-      let node = document.createElement('div')
-      document.body.appendChild(node)
-      /* eslint-disable no-new */
-      this.$$popover = createApp({
-        el: node,
-        render (createElement) {
-          return createElement(VxPopup, {
-            props: {
-              open: this.open,
-              histroy: this.histroy,
-              opacity: this.opacity
-            },
-            style: {
-              zIndex: 1000
-            },
-            on: {
-              'close': this.handleClose,
-              'after-close': this.handleAfterClose
-            }
-          }, [
-            createElement('div', {
-              class: ['vx-popover--content', popover.popoverClass],
-              style: {
-                opacity: '0'
-              },
-              slot: 'inner',
-              on: {
-                click: this.handleClose
-              }
-            }, [...popover.$slots.default])
-          ])
-        },
-        components: {VxPopup},
-        data () {
-          return {
-            open: false,
-            opacity: popover.overlayOpacity,
-            histroy: popover.histroy
-          }
-        },
-        mounted () {
-          this.open = true
-          this.$nextTick(() => {
-            let width = window.innerWidth
-            let height = window.innerHeight
-            let node = this.$el.querySelector('.vx-popover--content')
-            let rect = popover.$el.getBoundingClientRect()
-            let left = rect.left + 'px'
-            let right = 'auto'
-            if (rect.left > width / 2) {
-              left = 'auto'
-              right = width - rect.right + 'px'
-            }
-            let top = rect.bottom + 'px'
-            let bottom = 'auto'
-            if (rect.top > height / 2) {
-              top = 'auto'
-              bottom = height - rect.top + 'px'
-            }
-            requestAnimationFrame(() => {
-              node.style.cssText = `top:${top};left:${left};right:${right};bottom:${bottom};opacity:1`
-              left === 'auto' && node.classList.add('vx-popover--content-right')
-              top === 'auto' && node.classList.add('vx-popover--content-bottom')
-            })
-          })
-        },
-        beforeUnmount () {
-          this.$el.parentNode && this.$el.parentNode.removeChild(this.$el)
-        },
-        methods: {
-          handleClose () {
-            this.open = false
-          },
-          handleAfterClose () {
-            popover.$$popover.$destroy()
-          }
-        }
-      })
-      this.$emit('open')
-    },
-    handleClickPopover () {
-      if (this.$$popover) {
-        this.$$popover.open = false
-        this.$$popover.$destroy()
-        this.$$popover = null
+        })
+      },
+      handleClose () {
+        myOpen.value = false
+        emit('update:open', false)
       }
     }
   }
